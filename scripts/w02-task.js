23188172f92736ed06d0b849c3493897cf8b2f34
robertinhosud <u540151443@byui.edu>
2024-01-20 12:45:12 -0700

    w2 test final

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -8,7 +8,7 @@ let fullName = 'Roberto Soares'
 
 var currentYear = new Date().getFullYear();
 
-var profilePicture = 'images\IMG_7997.png';
+var profilePicture = 'images/IMG_7997.png';
 
 
 /* Step 3 - Element Variables */
@@ -19,36 +19,32 @@ const foodElement = document.getElementById('food')
 
 const yearElement =  document.querySelector('#year') 
 
-const imageElement = document.querySelector("#profileImage"); 
+const imageElement = document.querySelector('#home picture img');
 
 /* Step 4 - Adding Content */
 
-nameElement.innerHTML = '<strong>${fullName}</strong>';
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
 
-nameElement.textContent = currentYear;
+yearElement.textContent = currentYear;
 
-imageElement.setAttribute("src", profilePicture);
-
-const altText = `Profile image of ${nameElement.textContent}`;
-
-imageElement.setAttribute("alt", altText);
+imageElement.setAttribute('src', profilePicture);
 
 /* Step 5 - Array */
 
 const favoriteFoods = ["Pizza", "Chocolate", "Ice Cream", "Burger"];
 
-foodElement.innerHTML = favoriteFoods.join("<br>");
+foodElement.innerHTML = favoriteFoods.join();
 
 const newFavoriteFood = "Pasta";
 
 favoriteFoods.push(newFavoriteFood);
 
-foodElement.innerHTML += `<br>${favoriteFoods.join("<br>")}`;
+foodElement.innerHTML += `<br>${favoriteFoods.join()}`;
 
 favoriteFoods.shift();
 
-foodElement.innerHTML += `<br>${favoriteFoods.join("<br>")}`;
+foodElement.innerHTML += `<br>${favoriteFoods.join()}`;
 
 favoriteFoods.pop();
-
-foodElement.innerHTML += `<br>${favoriteFoods.join("<br>")}`;
+ 
+foodElement.innerHTML += `<br>${favoriteFoods.join()}`;
